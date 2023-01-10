d8aa963c53fae3e7baae7b3cf2d5d68a535f2c52
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2023-01-10 15:16:08 +0100

    [Espn] Deal with missing roster
    Fixes #140

--- soccerdata/espn.py
+++ soccerdata/espn.py
@@ -10,13 +10,13 @@ import pandas as pd
 import requests
 
 from ._common import BaseRequestsReader, make_game_id, standardize_colnames
-from ._config import DATA_DIR, NOCACHE, NOSTORE, TEAMNAME_REPLACEMENTS
+from ._config import DATA_DIR, NOCACHE, NOSTORE, TEAMNAME_REPLACEMENTS, logger
 
 # http://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/summary?event=513466
 # http://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/scoreboard?dates=20180901
 
-ESPN_DATADIR = DATA_DIR / 'ESPN'
-ESPN_API = 'http://site.api.espn.com/apis/site/v2/sports/soccer'
+ESPN_DATADIR = DATA_DIR / "ESPN"
+ESPN_API = "http://site.api.espn.com/apis/site/v2/sports/soccer"
 
 
 class ESPN(BaseRequestsReader):
@@ -92,24 +92,24 @@ class ESPN(BaseRequestsReader):
         -------
         pd.DataFrame
         """
-        urlmask = ESPN_API + '/{}/scoreboard?dates={}'
-        filemask = 'Schedule_{}_{}.json'
+        urlmask = ESPN_API + "/{}/scoreboard?dates={}"
+        filemask = "Schedule_{}_{}.json"
 
         df_list = []
         # Get match days
         for lkey, skey in itertools.product(self._selected_leagues.values(), self.seasons):
             if int(skey[:2]) > int(str(datetime.datetime.now().year + 1)[-2:]):
-                start_date = ''.join(['19', skey[:2], '07', '01'])
+                start_date = "".join(["19", skey[:2], "07", "01"])
             else:
-                start_date = ''.join(['20', skey[:2], '07', '01'])
+                start_date = "".join(["20", skey[:2], "07", "01"])
 
             url = urlmask.format(lkey, start_date)
             resp = requests.get(url=url)
             data = resp.json()
 
             match_dates = [
-                datetime.datetime.strptime(d, '%Y-%m-%dT%H:%MZ').strftime('%Y%m%d')
-                for d in data['leagues'][0]['calendar']
+                datetime.datetime.strptime(d, "%Y-%m-%dT%H:%MZ").strftime("%Y%m%d")
+                for d in data["leagues"][0]["calendar"]
             ]
             for date in match_dates:
                 url = urlmask.format(lkey, date)
@@ -121,25 +121,25 @@ class ESPN(BaseRequestsReader):
                 df_list.extend(
                     [
                         {
-                            'league': lkey,
-                            'season': skey,
-                            'date': e['date'],
-                            'home_team': e['competitions'][0]['competitors'][0]['team']['name'],
-                            'away_team': e['competitions'][0]['competitors'][1]['team']['name'],
-                            'game_id': int(e['id']),
-                            'league_id': lkey,
+                            "league": lkey,
+                            "season": skey,
+                            "date": e["date"],
+                            "home_team": e["competitions"][0]["competitors"][0]["team"]["name"],
+                            "away_team": e["competitions"][0]["competitors"][1]["team"]["name"],
+                            "game_id": int(e["id"]),
+                            "league_id": lkey,
                         }
-                        for e in data['events']
+                        for e in data["events"]
                     ]
                 )
         df = (
             pd.DataFrame(df_list)
             .pipe(self._translate_league)
-            .replace({'home_team': TEAMNAME_REPLACEMENTS, 'away_team': TEAMNAME_REPLACEMENTS})
-            .assign(date=lambda x: pd.to_datetime(x['date']))
-            .dropna(subset=['home_team', 'away_team', 'date'])
+            .replace({"home_team": TEAMNAME_REPLACEMENTS, "away_team": TEAMNAME_REPLACEMENTS})
+            .assign(date=lambda x: pd.to_datetime(x["date"]))
+            .dropna(subset=["home_team", "away_team", "date"])
             .assign(game=lambda df: df.apply(make_game_id, axis=1))
-            .set_index(['league', 'season', 'game'])
+            .set_index(["league", "season", "game"])
             .sort_index()
         )
 
@@ -162,8 +162,8 @@ class ESPN(BaseRequestsReader):
         -------
         pd.DataFrame.
         """
-        urlmask = ESPN_API + '/{}/summary?event={}'
-        filemask = 'Summary_{}.json'
+        urlmask = ESPN_API + "/{}/summary?event={}"
+        filemask = "Summary_{}.json"
 
         df_schedule = self.read_schedule().reset_index()
         if match_id is not None:
@@ -172,43 +172,45 @@ class ESPN(BaseRequestsReader):
             ]
             if len(iterator) == 0:
                 raise ValueError(
-                    'No games with the given IDs found for the selected seasons and leagues.'
+                    "No games with the given IDs found for the selected seasons and leagues."
                 )
         else:
             iterator = df_schedule
 
         df_list = []
         for i, match in iterator.iterrows():
-            url = urlmask.format(match['league_id'], match['game_id'])
-            filepath = self.data_dir / filemask.format(match['game_id'])
+            url = urlmask.format(match["league_id"], match["game_id"])
+            filepath = self.data_dir / filemask.format(match["game_id"])
             reader = self.get(url, filepath)
 
             data = json.load(reader)
             for i in range(2):
                 match_sheet = {
-                    'game': match['game'],
-                    'league': match['league'],
-                    'season': match['season'],
-                    'team': data['boxscore']['form'][i]['team']['displayName'],
-                    'is_home': (i == 0),
-                    'venue': data['gameInfo']['venue']['fullName']
-                    if 'venue' in data['gameInfo']
+                    "game": match["game"],
+                    "league": match["league"],
+                    "season": match["season"],
+                    "team": data["boxscore"]["form"][i]["team"]["displayName"],
+                    "is_home": (i == 0),
+                    "venue": data["gameInfo"]["venue"]["fullName"]
+                    if "venue" in data["gameInfo"]
                     else None,
-                    'attendance': data['gameInfo']['attendance'],
-                    'capacity': data['gameInfo']['venue']['capacity']
-                    if 'venue' in data['gameInfo']
+                    "attendance": data["gameInfo"]["attendance"],
+                    "capacity": data["gameInfo"]["venue"]["capacity"]
+                    if "venue" in data["gameInfo"]
                     else None,
-                    'roster': data['rosters'][i]['roster'],
+                    "roster": data["rosters"][i]["roster"]
+                    if "roster" in data["rosters"][i]
+                    else None,
                 }
-                if 'statistics' in data['boxscore']['teams'][i]:
-                    for stat in data['boxscore']['teams'][i]['statistics']:
-                        match_sheet[stat['name']] = stat['displayValue']
+                if "statistics" in data["boxscore"]["teams"][i]:
+                    for stat in data["boxscore"]["teams"][i]["statistics"]:
+                        match_sheet[stat["name"]] = stat["displayValue"]
                 df_list.append(match_sheet)
         df = (
             pd.DataFrame(df_list)
-            .replace({'team': TEAMNAME_REPLACEMENTS})
+            .replace({"team": TEAMNAME_REPLACEMENTS})
             .pipe(standardize_colnames)
-            .set_index(['league', 'season', 'game', 'team'])
+            .set_index(["league", "season", "game", "team"])
             .sort_index()
         )
         return df
@@ -232,8 +234,8 @@ class ESPN(BaseRequestsReader):
         -------
         pd.DataFrame.
         """
-        urlmask = ESPN_API + '/{}/summary?event={}'
-        filemask = 'Summary_{}.json'
+        urlmask = ESPN_API + "/{}/summary?event={}"
+        filemask = "Summary_{}.json"
 
         df_schedule = self.read_schedule().reset_index()
         if match_id is not None:
@@ -242,74 +244,85 @@ class ESPN(BaseRequestsReader):
             ]
             if len(iterator) == 0:
                 raise ValueError(
-                    'No games with the given IDs found for the selected seasons and leagues.'
+                    "No games with the given IDs found for the selected seasons and leagues."
                 )
         else:
             iterator = df_schedule
 
         df_list = []
         for i, match in iterator.iterrows():
-            url = urlmask.format(match['league_id'], match['game_id'])
-            filepath = self.data_dir / filemask.format(match['game_id'])
+            url = urlmask.format(match["league_id"], match["game_id"])
+            filepath = self.data_dir / filemask.format(match["game_id"])
             reader = self.get(url, filepath)
 
             data = json.load(reader)
             for i in range(2):
-                for p in data['rosters'][i]['roster']:
+                if "roster" not in data["rosters"][i]:
+                    logger.info(
+                        "No lineup info found for team %d in game with ID=%s",
+                        i + 1,
+                        match["game_id"],
+                    )
+                    continue
+                for p in data["rosters"][i]["roster"]:
                     match_sheet = {
-                        'game': match['game'],
-                        'league': match['league'],
-                        'season': match['season'],
-                        'team': data['boxscore']['form'][i]['team']['displayName'],
-                        'is_home': (i == 0),
-                        'player': p['athlete']['displayName'],
-                        'position': p['position']['name'] if 'position' in p else None,
-                        'formation_place': p['formationPlace'] if 'formationPlace' in p else None,
+                        "game": match["game"],
+                        "league": match["league"],
+                        "season": match["season"],
+                        "team": data["boxscore"]["form"][i]["team"]["displayName"],
+                        "is_home": (i == 0),
+                        "player": p["athlete"]["displayName"],
+                        "position": p["position"]["name"] if "position" in p else None,
+                        "formation_place": p["formationPlace"] if "formationPlace" in p else None,
                     }
 
-                    if p['starter']:
-                        match_sheet['sub_in'] = 'start'
-                    elif p['subbedIn']:
-                        ii = [i for i, x in enumerate(p['plays']) if x['substitution']][0]
-                        match_sheet['sub_in'] = sum(
+                    if p["starter"]:
+                        match_sheet["sub_in"] = "start"
+                    elif p["subbedIn"]:
+                        ii = [i for i, x in enumerate(p["plays"]) if x["substitution"]][0]
+                        match_sheet["sub_in"] = sum(
                             map(
                                 int,
                                 re.findall(
-                                    r'(\d{1,3})',
-                                    p['plays'][ii]['clock']['displayValue'],
+                                    r"(\d{1,3})",
+                                    p["plays"][ii]["clock"]["displayValue"],
                                 ),
                             )
                         )
                     else:
-                        match_sheet['sub_in'] = None
+                        match_sheet["sub_in"] = None
 
-                    if (p['starter'] or p['subbedIn']) and not p['subbedOut']:
-                        match_sheet['sub_out'] = 'end'
-                    elif p['subbedOut']:
-                        j = 0 if not p['subbedIn'] else 1
-                        ii = [i for i, x in enumerate(p['plays']) if x['substitution']][j]
-                        match_sheet['sub_out'] = sum(
+                    if (p["starter"] or p["subbedIn"]) and not p["subbedOut"]:
+                        match_sheet["sub_out"] = "end"
+                    elif p["subbedOut"]:
+                        j = 0 if not p["subbedIn"] else 1
+                        ii = [i for i, x in enumerate(p["plays"]) if x["substitution"]][j]
+                        match_sheet["sub_out"] = sum(
                             map(
                                 int,
                                 re.findall(
-                                    r'(\d{1,3})',
-                                    p['plays'][ii]['clock']['displayValue'],
+                                    r"(\d{1,3})",
+                                    p["plays"][ii]["clock"]["displayValue"],
                                 ),
                             )
                         )
                     else:
-                        match_sheet['sub_out'] = None
+                        match_sheet["sub_out"] = None
 
-                    if 'stats' in p:
-                        for stat in p['stats']:
-                            match_sheet[stat['name']] = stat['value']
+                    if "stats" in p:
+                        for stat in p["stats"]:
+                            match_sheet[stat["name"]] = stat["value"]
 
                     df_list.append(match_sheet)
+
+        if len(df_list) == 0:
+            return pd.DataFrame()
+
         df = (
             pd.DataFrame(df_list)
-            .replace({'team': TEAMNAME_REPLACEMENTS})
+            .replace({"team": TEAMNAME_REPLACEMENTS})
             .pipe(standardize_colnames)
-            .set_index(['league', 'season', 'game', 'team', 'player'])
+            .set_index(["league", "season", "game", "team", "player"])
             .sort_index()
         )
         return df

3724cf7a0f319079253cdc4802b2e144d286feb2
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2023-12-31 19:12:47 +0100

    Remove Unused "type: ignore" comments

--- soccerdata/_common.py
+++ soccerdata/_common.py
@@ -71,9 +71,9 @@ class BaseReader(ABC):
                 "https": "socks5://127.0.0.1:9050",
             }
         elif isinstance(proxy, dict):
-            self.proxy = lambda: proxy  # type: ignore
+            self.proxy = lambda: proxy
         elif isinstance(proxy, list):
-            self.proxy = lambda: random.choice(proxy)  # type: ignore
+            self.proxy = lambda: random.choice(proxy)
         elif callable(proxy):
             self.proxy = proxy
         else:

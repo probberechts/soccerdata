21d17f2d599a7acc6b7b9b541fd19562b7cc635d
Marc Baron <marcjbaron@gmail.com>
2024-01-29 10:38:13 -0500

    Update 'FotMob' name convention; add test decorator

--- tests/test_Fotmob.py
+++ tests/test_Fotmob.py
@@ -1,25 +1,28 @@
-"""Unittests for class soccerdata.Fotmob."""
+"""Unittests for class soccerdata.FotMob."""
 
 import pandas as pd
 import pytest
 
 # import soccerdata as sd
-from soccerdata.fotmob import Fotmob
+from soccerdata.fotmob import FotMob
 
 # Unittests -------------------------------------------------------------------
 
 
-def test_read_league_table(fotmob_laliga: Fotmob) -> None:
+@pytest.mark.fails_gha
+def test_read_league_table(fotmob_laliga: FotMob) -> None:
     assert isinstance(fotmob_laliga.read_league_table(), pd.DataFrame)
 
 
-def test_read_schedule(fotmob_laliga: Fotmob) -> None:
+@pytest.mark.fails_gha
+def test_read_schedule(fotmob_laliga: FotMob) -> None:
     assert isinstance(fotmob_laliga.read_schedule(), pd.DataFrame)
 
 
+@pytest.mark.fails_gha
 @pytest.mark.parametrize(
     "stat_type",
     ['Top stats', 'Shots', 'Expected goals (xG)', 'Passes', 'Defence', 'Duels', 'Discipline'],
 )
-def test_read_match_stats(fotmob_laliga: Fotmob, stat_type: str) -> None:
+def test_read_match_stats(fotmob_laliga: FotMob, stat_type: str) -> None:
     assert isinstance(fotmob_laliga.read_match_stats(stat_type, match_id="3424405"), pd.DataFrame)

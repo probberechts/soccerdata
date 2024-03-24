270f989155705ac8debb2645523403441527994f
ksbharaj <kbharaj3@gatech.edu>
2024-03-24 00:04:35 -0400

    Sofascore tests v1 added

--- tests/conftest.py
+++ tests/conftest.py
@@ -69,3 +69,15 @@ def understat_epl_1516() -> sd.Understat:
 def understat_epl_9091() -> sd.Understat:
     """Return a correctly initialized instance of Understat filtered by league: Premier League."""
     return sd.Understat("ENG-Premier League", "90-91")
+
+
+@pytest.fixture
+def sofascore_epl_1516() -> sd.Sofascore:
+    """Return a correctly initialized instance of Understat filtered by league: Premier League."""
+    return sd.Sofascore("ENG-Premier League", "15-16")
+
+
+@pytest.fixture
+def sofascore_epl_9091() -> sd.Sofascore:
+    """Return a correctly initialized instance of Understat filtered by league: Premier League."""
+    return sd.Sofascore("ENG-Premier League", "90-91")

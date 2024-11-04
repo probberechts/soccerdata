b999c5e64f7df94bb37c3a454a5802d7ce98bd05
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2024-11-04 15:53:38 +0100

    [FotMob] patch for new anti-scraping header (#745)
    FotMob now requires a "x-fm-req" value to be present in the header.
    
    Fixes #742

--- soccerdata/fotmob.py
+++ soccerdata/fotmob.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from typing import Callable, Optional, Union
 
 import pandas as pd
+import requests
 
 from ._common import BaseRequestsReader, make_game_id
 from ._config import DATA_DIR, NOCACHE, NOSTORE, TEAMNAME_REPLACEMENTS, logger
@@ -77,6 +78,17 @@ class FotMob(BaseRequestsReader):
             (self.data_dir / "leagues").mkdir(parents=True, exist_ok=True)
             (self.data_dir / "seasons").mkdir(parents=True, exist_ok=True)
             (self.data_dir / "matches").mkdir(parents=True, exist_ok=True)
+
+    def _init_session(self) -> requests.Session:
+        session = super()._init_session()
+        try:
+            r = requests.get("http://46.101.91.154:6006/")
+            r.raise_for_status()
+        except requests.exceptions.ConnectionError:
+            raise ConnectionError("Unable to connect to the session cookie server.")
+        result = r.json()
+        session.headers.update(result)
+        return session
 
     @property
     def leagues(self) -> list[str]:

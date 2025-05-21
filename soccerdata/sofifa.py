515d3ec79e8c367ac0c20c214f8a010cb5626c73
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2025-05-21 15:37:07 +0200

    chore: replace undetected_chromedriver by seleniumbase (#846)
    * undetected chromedriver -> seleniumbase
    * remove some left traces of requests
    * proxy argmument is now a string instead of a dict

--- soccerdata/sofifa.py
+++ soccerdata/sofifa.py
@@ -36,19 +36,12 @@ class SoFIFA(BaseRequestsReader):
         (e.g., 230034). Alternatively, the string "all" can be used to include all
         versions and "latest" to include the latest version only. Defaults to
         "latest".
-    proxy : 'tor' or dict or list(dict) or callable, optional
+    proxy : 'tor' or or dict or list(dict) or callable, optional
         Use a proxy to hide your IP address. Valid options are:
             - "tor": Uses the Tor network. Tor should be running in
               the background on port 9050.
-            - dict: A dictionary with the proxy to use. The dict should be
-              a mapping of supported protocols to proxy addresses. For example::
-
-                  {
-                      'http': 'http://10.10.1.10:3128',
-                      'https': 'http://10.10.1.10:1080',
-                  }
-
-            - list(dict): A list of proxies to choose from. A different proxy will
+            - str: The address of the proxy server to use.
+            - list(str): A list of proxies to choose from. A different proxy will
               be selected from this list after failed requests, allowing rotating
               proxies.
             - callable: A function that returns a valid proxy. This function will
@@ -65,9 +58,7 @@ class SoFIFA(BaseRequestsReader):
         self,
         leagues: Optional[Union[str, list[str]]] = None,
         versions: Union[str, int, list[int]] = "latest",
-        proxy: Optional[
-            Union[str, dict[str, str], list[dict[str, str]], Callable[[], dict[str, str]]]
-        ] = None,
+        proxy: Optional[Union[str, list[str], Callable[[], str]]] = None,
         no_cache: bool = NOCACHE,
         no_store: bool = NOSTORE,
         data_dir: Path = SO_FIFA_DATADIR,

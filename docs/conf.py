412af4d19b9f399635a77d1bcbd426cfb4c46d2e
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2026-01-16 17:35:51 +0100

    Bump version: 1.8.7 → 1.8.8

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ author = "Pieter Robberechts"
 copyright = f"2021, {author}"  # noqa: A001
 
 # The full version, including alpha/beta/rc tags
-release = "1.8.7"
+release = "1.8.8"
 
 # -- General configuration ------------------------------------------------
 

b9143f3ff3434b63b0f03fe0f57fd151f7be0a14
Sai Asish Y <say.apm35@gmail.com>
2026-06-12 05:32:56 -0700

    fix(whoscored): unwrap JSON wrapped in HTML <pre> from page_source (#944)
    Signed-off-by: SAY-5 <say.apm35@gmail.com>
    Co-authored-by: Pieter Robberechts <pieter.robberechts@kuleuven.be>

--- tests/test_Whoscored.py
+++ tests/test_Whoscored.py
@@ -1,6 +1,10 @@
 """Unittests for class soccerdata.WhoScored."""
 
+from unittest.mock import MagicMock
+
 import pandas as pd
+
+from soccerdata.whoscored import WhoScored
 
 # Unittests -------------------------------------------------------------------
 
@@ -11,3 +15,19 @@ def test_whoscored_missing_players(whoscored):
 
 def test_whoscored_events(whoscored):
     assert isinstance(whoscored.read_events(1485184), pd.DataFrame)
+
+
+def test_validate_page_unwraps_json_in_pre():
+    instance = WhoScored.__new__(WhoScored)
+    driver = MagicMock()
+    driver.page_source = '<html><body><pre>{"tournaments": []}</pre></body></html>'
+    instance._driver = driver
+    assert instance._validate_page("http://example") == '{"tournaments": []}'
+
+
+def test_validate_page_passes_through_html():
+    instance = WhoScored.__new__(WhoScored)
+    driver = MagicMock()
+    driver.page_source = "<html><body><h1>hi</h1></body></html>"
+    instance._driver = driver
+    assert instance._validate_page("http://example") == driver.page_source

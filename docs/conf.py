323169a3acc1378cc8c5318db3bae7de5fe3d14f
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2026-07-24 14:21:08 +0200

    Bump version: 1.9.0 → 1.9.1

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ author = "Pieter Robberechts"
 copyright = f"2021, {author}"
 
 # The full version, including alpha/beta/rc tags
-release = "1.9.0"
+release = "1.9.1"
 
 # -- General configuration ------------------------------------------------
 
